93c343a1e15ad54410ae2b78b89ea461eba0a475
infl4me <hellpl4y@gmail.com>
2019-01-01 02:12:17 +0500

    complete step 1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-import program from '..';
+import program from 'commander';
+import { version } from '../../package.json';
 
-program.parse(process.argv);
+program
+  .version(version)
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const program = require('commander');
-
-export default program
-  .version('0.0.1')
-  .arguments('<firstConfig> <secondConfig>')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format');
