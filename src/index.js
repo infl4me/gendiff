29cba77ebc07f68e24d8a015f080032fd308d0a0
infl4me <hellpl4y@gmail.com>
2019-01-02 00:27:17 +0500

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { has } from 'lodash';
 import fs from 'fs';
 
+const buildUnchanged = (key, value) => `  ${key}: ${value}`;
+const buildChanged = (key1, value1, key2, value2) => `- ${key1}: ${value1}\n+ ${key2}: ${value2}`;
+const buildDeleted = (key, value) => `- ${key}: ${value}`;
+const buildAdded = (key, value) => `+ ${key}: ${value}`;
+
+
 const actions = {
-  unchanged: ([key, value]) => `  ${key}: ${value}`,
-  changed: ([[key1, value1], [key2, value2]]) => `- ${key1}: ${value1}\n+ ${key2}: ${value2}`,
-  deleted: ([key, value]) => `- ${key}: ${value}`,
-  added: ([key, value]) => `+ ${key}: ${value}`,
+  unchanged: ([k, v]) => buildUnchanged(k, v),
+  changed: ([[k1, v1], [k2, v2]]) => buildChanged(k1, v1, k2, v2),
+  deleted: ([k, v]) => buildDeleted(k, v),
+  added: ([k, v]) => buildAdded(k, v),
 };
 
 const parse = (file1, file2) => {
