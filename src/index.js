1c26e0cd71655928dafac36b7d2ccf991c8b71de
infl4me <hellpl4y@gmail.com>
2019-01-02 07:13:10 +0500

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -6,47 +6,58 @@ const buildChanged = (key1, value1, key2, value2) => `- ${key1}: ${value1}\n+ ${
 const buildDeleted = (key, value) => `- ${key}: ${value}`;
 const buildAdded = (key, value) => `+ ${key}: ${value}`;
 
-
 const actions = {
-  unchanged: ([k, v]) => buildUnchanged(k, v),
-  changed: ([[k1, v1], [k2, v2]]) => buildChanged(k1, v1, k2, v2),
-  deleted: ([k, v]) => buildDeleted(k, v),
-  added: ([k, v]) => buildAdded(k, v),
+  unchanged: {
+    getEntry: (key, value) => [key, value],
+    buildStr: ([k, v]) => buildUnchanged(k, v),
+    check: (obj, key, value) => has(obj, key) && value === obj[key],
+  },
+  changed: {
+    getEntry: (key, value1, value2) => [[key, value1], [key, value2]],
+    buildStr: ([[k1, v1], [k2, v2]]) => buildChanged(k1, v1, k2, v2),
+    check: (obj, key) => has(obj, key),
+  },
+  deleted: {
+    getEntry: (key, value) => [key, value],
+    buildStr: ([k, v]) => buildDeleted(k, v),
+    check: (obj, key) => !has(obj, key),
+  },
+  added: {
+    getEntry: (key, value) => [key, value],
+    buildStr: ([k, v]) => buildAdded(k, v),
+  },
 };
 
-const parseToAst = (object1, object2) => {
-  const entriesobject1 = Object.entries(object1);
-  const keysobject2 = Object.keys(object2);
+const parseToAst = (obj1, obj2) => {
+  const keysObj1 = Object.keys(obj1);
+  const keysObj2 = Object.keys(obj2);
 
-  const reduce1 = entriesobject1.reduce((acc, [key, value]) => {
-    if (has(object2, key)) {
-      if (value === object2[key]) {
-        return [...acc, { flag: 'unchanged', entry: [key, value] }];
-      }
-      return [...acc, { flag: 'changed', entry: [[key, value], [key, object2[key]]] }];
-    }
-    return [...acc, { flag: 'deleted', entry: [key, value] }];
+  const reduce1 = keysObj1.reduce((acc, key) => {
+    const found = Object.keys(actions).find(n => actions[n].check(obj2, key, obj1[key]));
+    return [...acc, { flag: found, entry: actions[found].getEntry(key, obj1[key], obj2[key]) }];
   }, []);
-  const reduce2 = keysobject2.reduce((acc, key) => (
-    has(object1, key) ? acc : [...acc, { flag: 'added', entry: [key, object2[key]] }]
+
+  const reduce2 = keysObj2.reduce((acc, key) => (
+    has(obj1, key) ? acc : [...acc, { flag: 'added', entry: [key, obj2[key]] }]
   ), reduce1);
+
   return reduce2;
 };
 
-export const render = (object1, object2) => {
-  const result = parseToAst(object1, object2)
-    .reduce((acc, n) => {
-      const { flag, entry } = n;
-      return `${acc}\n${actions[flag](entry)}`;
-    }, '');
+export const render = (obj1, obj2) => {
+  const ast = parseToAst(obj1, obj2);
+  const result = ast.reduce((acc, n) => {
+    const { flag, entry } = n;
+    return `${acc}\n${actions[flag].buildStr(entry)}`;
+  }, '');
+
   return `{${result}\n}`;
 };
 
-
 const gendiff = (pathFile1, pathFile2) => {
-  const object1 = parseFileToObject(pathFile1);
-  const object2 = parseFileToObject(pathFile2);
-
-  return render(object1, object2);
+  const obj1 = parseFileToObject(pathFile1);
+  const obj2 = parseFileToObject(pathFile2);
+  return render(obj1, obj2);
 };
+
 export default gendiff;
