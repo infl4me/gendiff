30680aa6d4cbfe95fc5fac13c20e0a9e099c68da
infl4me <hellpl4y@gmail.com>
2019-01-03 08:52:53 +0500

    add tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,23 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const defaultPath = './__tests__/__fixtures__/nested/';
-const defaultPathBefore = `${defaultPath}before`;
-const defaultPathAfter = `${defaultPath}after`;
-const expected = fs.readFileSync(`${defaultPath}expected.txt`, 'utf-8');
+const defaultPath = './__tests__/__fixtures__/';
+
 test.each([
-  ['.json', '.json'],
-  ['.yml', '.yml'],
-  ['.ini', '.ini'],
-])('test before%s and after%s',
-  (before, after) => {
-    const beforePath = `${defaultPathBefore}${before}`;
-    const afterPath = `${defaultPathAfter}${after}`;
+  ['.json', '.json', 'plain/'],
+  ['.yml', '.yml', 'plain/'],
+  ['.ini', '.ini', 'plain/'],
+  ['.json', '.ini', 'plain/'],
+  ['.yml', '.ini', 'plain/'],
+  ['.json', '.json', 'nested/'],
+  ['.yml', '.yml', 'nested/'],
+  ['.ini', '.ini', 'nested/'],
+  ['.json', '.ini', 'nested/'],
+  ['.yml', '.ini', 'nested/'],
+])('test before%s and after%s %s',
+  (before, after, dataType) => {
+    const beforePath = `${defaultPath}${dataType}before${before}`;
+    const afterPath = `${defaultPath}${dataType}after${after}`;
+    const expected = fs.readFileSync(`${defaultPath}${dataType}expected.txt`, 'utf-8');
     expect(gendiff(beforePath, afterPath)).toBe(expected);
   });
