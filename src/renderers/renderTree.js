d09368caf5b8bd08908230620bf9c8085ab3c0c3
infl4me <hellpl4y@gmail.com>
2019-01-06 06:23:57 +0500

    refactor renderTree

--- src/renderers/renderTree.js
+++ src/renderers/renderTree.js
@@ -1,9 +1,7 @@
 import { flatten } from 'lodash';
 
-const indent = '  ';
-
-const calcIndent = (depth, startFrom = 2) => (
-  indent.repeat(depth + (startFrom <= -1 ? -1 : startFrom / 2 - 1)));
+const calcIndent = (depth, size, initialDepth = 1) => (
+  '  '.repeat((depth - initialDepth + 1) + (size / 2 - 1)));
 
 const stringify = (value, depth) => {
   if (typeof value !== 'object') {
@@ -11,15 +9,15 @@ const stringify = (value, depth) => {
   }
   const keys = Object.keys(value);
   const result = keys.reduce((acc, key) => `${acc}${calcIndent(depth, 4)}${key}: ${value[key]}`, '');
-  return `{\n${result}\n${calcIndent(depth)}}`;
+  return `{\n${result}\n${calcIndent(depth, 2)}}`;
 };
 
-const buildNested = (key, children, depth, renderFunction) => `${calcIndent(depth)}${key}: ${renderFunction(children, depth + 1)}`;
-const buildUnchanged = (key, oldValue, depth) => `${calcIndent(depth, -1)}  ${key}: ${stringify(oldValue, depth)}`;
+const buildNested = (key, children, depth, renderFunction) => `${calcIndent(depth, 2)}${key}: ${renderFunction(children, depth + 1)}`;
+const buildUnchanged = (key, oldValue, depth) => `${calcIndent(depth, 2, 2)}  ${key}: ${stringify(oldValue, depth)}`;
 const buildChanged = (key, oldValue, newValue, depth) => (
-  [`${calcIndent(depth, -1)}- ${key}: ${stringify(oldValue, depth)}`, `${calcIndent(depth, -1)}+ ${key}: ${stringify(newValue, depth)}`]);
-const buildDeleted = (key, oldValue, depth) => `${calcIndent(depth, -1)}- ${key}: ${stringify(oldValue, depth)}`;
-const buildAdded = (key, newValue, depth) => `${calcIndent(depth, -1)}+ ${key}: ${stringify(newValue, depth)}`;
+  [`${calcIndent(depth, 2, 2)}- ${key}: ${stringify(oldValue, depth)}`, `${calcIndent(depth, 2, 2)}+ ${key}: ${stringify(newValue, depth)}`]);
+const buildDeleted = (key, oldValue, depth) => `${calcIndent(depth, 2, 2)}- ${key}: ${stringify(oldValue, depth)}`;
+const buildAdded = (key, newValue, depth) => `${calcIndent(depth, 2, 2)}+ ${key}: ${stringify(newValue, depth)}`;
 
 const actions = {
   nested: ({ name, children }, depth, renderFunction) => (
@@ -35,7 +33,7 @@ const render = (ast, depth = 1) => {
     const { type } = node;
     return actions[type](node, depth, render);
   });
-  return `{\n${flatten(result).join('\n')}\n${calcIndent(depth, -1)}}`;
+  return `{\n${flatten(result).join('\n')}\n${calcIndent(depth, 2, 2)}}`;
 };
 
 export default render;
