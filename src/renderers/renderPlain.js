ea01a3fef56709bb0e36633c7bb68177d9a3061a
infl4me <hellpl4y@gmail.com>
2019-01-06 18:40:19 +0500

    fix mentor's notes

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,3 +1,5 @@
+import { flatten } from 'lodash';
+
 const stringify = (value) => {
   if (typeof value === 'boolean') return value;
   if (typeof value === 'object') {
@@ -6,26 +8,36 @@ const stringify = (value) => {
   return Number.isNaN(Number(value)) ? `'${value}'` : value;
 };
 
-const buildChanged = (ancestry, oldValue, newValue) => `Property '${ancestry.join('.')}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
-const buildDeleted = ancestry => `Property '${ancestry.join('.')}' was removed`;
-const buildAdded = (ancestry, newValue) => `Property '${ancestry.join('.')}' was added with value: ${stringify(newValue)}`;
-
+const buildChanged = (ancestry, oldValue, newValue) => [`Property '${ancestry}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`];
+const buildDeleted = ancestry => [`Property '${ancestry}' was removed`];
+const buildAdded = (ancestry, newValue) => [`Property '${ancestry}' was added with value: ${stringify(newValue)}`];
+const joinStr = (str, delimiter) => `${str}${delimiter}`;
 const actions = {
-  nested: ({ children }, ancestry, fn) => fn(children, ancestry),
-  unchanged: () => '',
+  nested: ({ children }, ancestry, fn) => fn(children, joinStr(ancestry, '.')),
+  unchanged: () => [],
   changed: ({ oldValue, newValue }, ancestry) => buildChanged(ancestry, oldValue, newValue),
   deleted: (_a, ancestry) => buildDeleted(ancestry),
   added: ({ newValue }, ancestry) => buildAdded(ancestry, newValue),
 };
 
-const render = (ast, ancestry = []) => {
-  const result = ast.reduce((acc, node) => {
+const render = (ast, ancestry = '') => {
+  const result = ast.map((node) => {
     const { name, type } = node;
-    const newAncestry = [...ancestry, name];
-    const buildedString = actions[type](node, newAncestry, render);
-    return buildedString ? [...acc, buildedString] : acc;
-  }, []);
-  return result.join('\n');
+    const newAncestry = `${ancestry}${name}`;
+    return actions[type](node, newAncestry, render);
+  });
+  return flatten(result).join('\n');
 };
 
 export default render;
+
+// conconst render = (ast, ancestry = '') => {
+//   const result = ast.reduce((acc, node) => {
+//     const { name, type } = node;
+//     const newAncestry = `${ancestry}${ancestry ? '.' : ''}${name}`;
+//     const buildedString = actions[type](node, newAncestry, render);
+//     return buildedString ? [...acc, buildedString] : acc;
+//   }, []);
+
+//   return result.join('\n');
+// };
