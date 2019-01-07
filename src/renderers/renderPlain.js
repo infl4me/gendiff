5d7526311862beab72c76c081199e36933890177
infl4me <hellpl4y@gmail.com>
2019-01-07 05:18:59 +0500

    refactor renderPlain

--- src/renderers/renderPlain.js
+++ src/renderers/renderPlain.js
@@ -1,4 +1,4 @@
-import { flatten, isNaN, toNumber } from 'lodash';
+import { isNaN, toNumber } from 'lodash';
 
 const stringify = (value) => {
   if (typeof value === 'boolean') return value;
@@ -16,30 +16,30 @@ const buildAdded = (ancestry, newValue) => `Property '${ancestry}' was added wit
 const joinStr = (str, delimiter) => `${str}${delimiter}`;
 const actions = {
   nested: ({ children }, ancestry, fn) => fn(children, joinStr(ancestry, '.')),
-  unchanged: () => [],
+  unchanged: () => '',
   changed: ({ oldValue, newValue }, ancestry) => buildChanged(ancestry, oldValue, newValue),
   deleted: (_a, ancestry) => buildDeleted(ancestry),
   added: ({ newValue }, ancestry) => buildAdded(ancestry, newValue),
 };
 
+// const render = (ast, ancestry = '') => {
+//   const result = ast.map((node) => {
+//     const { name, type } = node;
+//     const newAncestry = `${ancestry}${name}`;
+//     return actions[type](node, newAncestry, render);
+//   });
+//   return flatten(result).join('\n');
+// };
+
 const render = (ast, ancestry = '') => {
-  const result = ast.map((node) => {
+  const result = ast.reduce((acc, node) => {
     const { name, type } = node;
     const newAncestry = `${ancestry}${name}`;
-    return actions[type](node, newAncestry, render);
-  });
-  return flatten(result).join('\n');
+    const buildedString = actions[type](node, newAncestry, render);
+    return buildedString ? [...acc, buildedString] : acc;
+  }, []);
+
+  return result.join('\n');
 };
 
 export default render;
-
-// conconst render = (ast, ancestry = '') => {
-//   const result = ast.reduce((acc, node) => {
-//     const { name, type } = node;
-//     const newAncestry = `${ancestry}${ancestry ? '.' : ''}${name}`;
-//     const buildedString = actions[type](node, newAncestry, render);
-//     return buildedString ? [...acc, buildedString] : acc;
-//   }, []);
-
-//   return result.join('\n');
-// };
